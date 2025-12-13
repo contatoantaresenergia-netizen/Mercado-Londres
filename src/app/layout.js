02bc4d909590bbda9a6804d39c0cc6da7257dce3
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2025-12-13 12:52:23 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'
-import { CartProvider } from '@/app/context/CartContext'
+import { CartProvider } from '@/app/context/cartContext'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
