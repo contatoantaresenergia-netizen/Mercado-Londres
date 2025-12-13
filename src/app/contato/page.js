f6c9dc40eb77e43468abb25f1849d2eec4d03923
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2025-12-13 19:04:48 +0100

    Update page.js

--- src/app/contato/page.js
+++ src/app/contato/page.js
@@ -271,7 +271,7 @@ export default function ContatoPage() {
                 <p className="text-gray-600">
                   Sim! Toda nossa equipe fala português fluentemente. 
                   Atendemos brasileiros em Londres com todo carinho!
-                </p>
+              </p>
               </div>
             </div>
           </div>
