b041d3ffc3745a1a687196a79e7249ddf9a8845f
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2025-12-15 15:48:10 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  // Adicione esta configuração
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 }
 
 module.exports = nextConfig
