ac88498ee9d82ae6f1a1a34d7af695256e814706
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2025-12-15 22:09:19 +0100

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  'https://vpqevrxwiglfpyrwxmne.supabase.co',
-  'vpqevrxwiglfpyrwxmne' 
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseKey) {
+  console.error('Atenção: As chaves do Supabase não foram encontradas nas variáveis de ambiente.')
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
