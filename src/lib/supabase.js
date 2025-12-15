af6055cdd63c89f23b8f03c4f31cbdf3c1619a78
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2025-12-15 22:17:43 +0100

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,11 @@
-const { createClient } = require('@supabase/supabase-js')
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
+// Esta verificação ajuda a não quebrar o Build se as chaves não forem lidas
 if (!supabaseUrl || !supabaseKey) {
-  console.warn('Supabase URL ou Key faltando nas variaveis de ambiente!')
+  console.error('ERRO: NEXT_PUBLIC_SUPABASE_URL ou NEXT_PUBLIC_SUPABASE_ANON_KEY não configurados!')
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
