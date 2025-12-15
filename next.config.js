14ecef25ab16f84fbf313c24c51f3d03f20ecbf4
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2025-12-15 16:51:10 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
-  },
-  // Adicione esta configuração
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'your-project-id.supabase.co', // Substitua pelo seu ID do projeto
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
   },
 }
 
