6b68dea37170d6e78c120f96d94d26670df57807
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2025-12-16 05:37:33 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    // Remova isrMemoryCacheSize - não é uma opção válida no Next.js 14
+  },
 }
 
 module.exports = nextConfig
+```
+
+### 2. Verifique o Arquivo `.vercelignore`
+
+Certifique-se de que você não está ignorando acidentalmente o diretório `.next`:
+```
+# .vercelignore NÃO deve incluir:
+# .next
+# .next/**
