ceeb23b7bb4b3cdc3d3ac036bb4e43c220e4d49c
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2025-12-17 15:17:13 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { CartProvider } from '@/app/context/CartContext'
 import Header from '@/app/components/Header'
 import Footer from '@/app/components/Footer'
+import WhatsAppButton from '@/app/components/WhatsAppButton' // Importando o botão
 
 export default function RootLayout({ children }) {
   return (
@@ -9,8 +10,11 @@ export default function RootLayout({ children }) {
       <body>
         <CartProvider>
           <Header />
-          {children}
+          <main>
+            {children}
+          </main>
           <Footer />
+          <WhatsAppButton /> {/* O botão flutuante entra aqui */}
         </CartProvider>
       </body>
     </html>
