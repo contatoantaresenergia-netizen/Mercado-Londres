a08bdfa121bc053ce33e3448b78e4f4e213db1a8
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2025-12-19 19:17:34 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,8 +8,8 @@ export const metadata = {
   title: 'Prime Brasil Market',
   description: 'O melhor mercado brasileiro em Londres',
   icons: {
-    icon: 'https://vpqevrxwiglfpyrwxmne.supabase.co/storage/v1/object/public/images/fav.icon/FB2A4E64-3EA5-46AA-8C66-35BE402E3BA7_4_5005_c.jpeg',
-    apple: 'https://vpqevrxwiglfpyrwxmne.supabase.co/storage/v1/object/public/images/fav.icon/FB2A4E64-3EA5-46AA-8C66-35BE402E3BA7_4_5005_c.jpeg',
+    icon: 'https://vpqevrxwiglfpyrwxmne.supabase.co/storage/v1/object/public/images/fav.icon/Gemini_Generated_Image_t78o5it78o5it78o-removebg-preview.png',
+    apple: 'https://vpqevrxwiglfpyrwxmne.supabase.co/storage/v1/object/public/images/fav.icon/Gemini_Generated_Image_t78o5it78o5it78o-removebg-preview.png',
   },
 }
 
