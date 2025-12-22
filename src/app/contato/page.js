feffccc5fcac8e704f85f7b4452e7427eb5a4835
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2025-12-22 09:47:10 +0100

    Update page.js

--- src/app/contato/page.js
+++ src/app/contato/page.js
@@ -120,9 +120,17 @@ export default function ContatoPage() {
                 </div>
               </div>
 
-              <div className="mt-8 bg-gray-200 rounded-lg h-64 flex items-center justify-center">
-                <p className="text-gray-500">🗺️ Mapa - Londres, UK</p>
-              </div>
+            <div className="mt-8 bg-gray-200 rounded-lg h-64 overflow-hidden">
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158858.1106774!2d-0.241681!3d51.5287352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondres%2C%20Reino%20Unido!5e0!3m2!1spt-PT!2spt!4v1710000000000"
+    width="100%"
+    height="100%"
+    style={{ border: 0 }}
+    allowFullScreen=""
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+  ></iframe>
+</div>
             </div>
 
             <div>
