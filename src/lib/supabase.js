9ee47f2ca5991d7c39be7e626e4f78f1fef8b90f
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2025-12-30 08:31:25 +0100

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://vpqevrxwiglfpyrwxmne.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sbp_81bedd5e8f709ad6c60bb11a92994a77cfa4dbc4'
+// O Next.js busca os valores que você configurou no painel da Vercel
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
