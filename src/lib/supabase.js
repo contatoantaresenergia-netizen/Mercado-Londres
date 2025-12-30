d63a1bcc005a2c40500a5d565904de04cc3ce1b8
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2025-12-30 08:37:30 +0100

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// No JavaScript, apenas chamamos a variável. 
-// Se ela não existir, o erro aparecerá no console de forma clara.
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
